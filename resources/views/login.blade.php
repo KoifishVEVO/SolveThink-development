3d015874ace01afa93c3debf3cfd39deddbdb51e
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2025-03-14 18:08:54 +0700

    frontend update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,74 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: #272780;
-            height: 100vh;
-        }
-        .left-panel {
-            
-            height: 100%;
-        }
-        .login-container {
-            max-width: 170vh;
-            height: 80vh;
-        }
-        .form-control {
-            padding: 0.75rem 1.5rem;
-        }
-        .btn-custom-primary:hover {
-            background-color: #272780 !important;
-            border-color: #272780 !important;
-        }
-        .btn-custom-primary {
-            background-color: #272780 !important;
-            border-color: #272780 !important;
-            color: #fff !important;
-            font-size: 20px;
-        }
-        ::placeholder{
-            color: #89A1D0 !important;
-            font-size: 14px;
-        }
-        .form-header-container {
-            margin-bottom: 4rem;
-            margin-top: -1rem;
-        }
-        .bottomtext a {
-            color: #272780;
-            text-decoration: none; 
-        }
-        .bottomtext {
-            color: #272780;
-            text-decoration: none; 
-        }
-        .left-shadow {
-            box-shadow: -5px 0 5px rgba(0, 0, 0, 0.2);
-        }
-        .alert-text{
-            font-size: 14px;
-            color: red;
-            padding: 1px;
-        }
-    </style>
-</head>
-<body class="d-flex align-items-center justify-content-center">
-    <div class="container login-container">
+@extends('layouts.header')
+
+@section('title') 
+    Login
+@endsection
+
+@section('content')
+    <div class="container login-container" >
         <div class="row h-100 bg-white shadow">
-            <!-- Left Panel with Logo -->
-            <div class="col-md-5 d-flex align-items-center justify-content-center bg-white left-panel">
-                <div class="text-center">
-                <img src="{{asset('assets/images/solvethink_transparent.png')}}" alt="SolveThink Logo" class="mb-2 img-fluid max-width: 100%">
+            <!-- Left Panel(logo) -->
+            <div class="col-md-5 d-none d-md-flex bg-white p-0">
+                <div class="d-flex align-items-center justify-content-center w-100 h-100">
+                    <img src="{{ asset('assets/images/solvethink_transparent.png') }}" alt="SolveThink Logo"
+                        class="img-fluid" style="max-width: 100%;">
                 </div>
             </div>
-            
-            <!-- Right Panel with Form -->
-            <div class="col-md-7 p-5 d-flex flex-column justify-content-center left-shadow">
+
+            <!-- Right Panel(form)-->
+            <div class="col-md-7  p-5 d-flex flex-column justify-content-center left-shadow">
                 <div class="form-header-container text-center">
                     <h1 class="fw-bold display-4">LOGIN</h1>
                 </div>
@@ -84,22 +32,20 @@
 
                    
                     @if (session('error'))
-                    <div class="alert alert-text text-center p2 small" role="alert">
-                    {{ session('error') }}
-                    </div>
+                        <div class="alert alert-text text-center p2 small" role="alert">
+                            {{ session('error') }}
+                        </div>
                     @endif
-                
-                    <div class="d-grid gap-2 mt-4">
+
+                    <div class="d-grid gap-2  pt-md-4" style="padding-top: 9rem;">
                         <button type="submit" class="btn btn-custom-primary rounded-pill py-2 fw-bold">MASUK</button>
                     </div>
                 </form>
-                <div class="text-center mt-3 bottomtext">
+                <div class="text-center mt-3 bottomtext fs-sm fs-md">
                     <span>Belum punya akun? <a href="{{ url('/register') }}" class="fw-bold">Register!</a></span>
                 </div>
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+    @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,78 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: #272780;
-            height: 100vh;
-        }
-        .left-panel {
-          
-            height: 100%;
-            
-        }
-        .login-container {
-            max-width: 170vh;
-            height: 80vh;
-        }
-        .form-control {
-            padding: 0.75rem 1.5rem;
-        }
-        .btn-custom-primary:hover {
-            background-color: #272780 !important;
-            border-color: #272780 !important;
-        }
-        .btn-custom-primary {
-            background-color: #272780 !important;
-            border-color: #272780 !important;
-            color: #fff !important;
-            font-size: 20px;
-        }
-        ::placeholder{
-            color: #89A1D0 !important;
-            font-size: 14px;
-        }
-        .form-header-container {
-            margin-bottom: 4rem;
-            margin-top: -3rem;
-        }
-        .bottomtext a {
-            color: #272780;
-            text-decoration: none; 
-        }
-        .bottomtext  {
-            color: #272780;
-            text-decoration: none; 
-        }
-        .left-shadow{
-            box-shadow: -5px 0 5px rgba(0, 0, 0, 0.2);
-        }
-        .alert-text{
-            font-size: 14px;
-            color: red;
-            padding: 1px;
-        }
-    </style>
-</head>
-<body class="d-flex align-items-center justify-content-center">
+@extends('layouts.header')
+
+@section('title') 
+    Register
+@endsection
+
+@section('content')
     <div class="container login-container">
         <div class="row h-100 bg-white shadow">
-            <!-- Left Panel with Logo -->
-            <div class="col-md-5 d-flex align-items-center justify-content-center bg-white left-panel">
+            <!-- Left Panel(logo) -->
+            <div class="col-md-5 d-flex align-items-center justify-content-center bg-white left-panel d-none d-lg-flex">
+
                 <div class="text-center">
                     <img src="{{asset('assets/images/solvethink_transparent.png')}}" alt="SolveThink Logo" class="mb-2 img-fluid max-width: 100%">
 
                 </div>
             </div>
-            
-            <!-- Right Panel with Form -->
-            <div class="col-md-7 p-5 d-flex flex-column justify-content-center left-shadow">
+
+            <!-- Right Panel(form)-->
+            <div class="col-md-12 col-lg-7 col-sm-12 p-2 d-flex flex-column justify-content-center left-shadow">
                 <div class="form-header-container text-center">
-                    <h1 class="fw-bold display-4">Buat Akun Baru</h1>
+                    <h1 class="fw-bold h-2 ">Buat Akun Baru</h1>
                 </div>
                 <form method="POST" action = "{{ url('/register') }}">
                     @csrf
@@ -107,7 +54,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+    @endsection
