4918c48f1df44f27fb14677044576ad6f94d7b80
AdrianIchiro <adrianfahren10@gmail.com>
2025-04-27 19:24:10 +0700

    get data

--- app/Http/Controllers/penyewaanBarangController.php
+++ app/Http/Controllers/penyewaanBarangController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class penyewaanBarangController extends Controller
 {
     public function index()
     {
-        
-        return view('penyewaanBarang');
+        $response = Http::get('https://rental.solvethink.id/api/penyewaan-komponen-solvethink');
+        $data = $response->json();
+
+        $penyewaan = collect($data['data']);
+
+        return view('penyewaanBarang', compact('penyewaan'));
     }
 }
