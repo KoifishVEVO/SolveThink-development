77e707c097250134c0c1b54c5472c3f7d0d021e2
AdrianIchiro <adrianfahren10@gmail.com>
2025-04-28 14:41:44 +0700

    back pembeliaan

--- app/Http/Controllers/penjualanBarangController.php
+++ app/Http/Controllers/penjualanBarangController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class penjualanBarangController extends Controller
 {
     public function index()
     {
-        
-        return view('penjualanBarang');
+        $response = Http::withoutVerifying()->get('https://sale.solvethink.id/api/penjualan-komponen-solvethink');
+            $data = $response->json();
+
+            $penjualan = collect($data['data']);
+
+        return view('penjualanBarang', compact('penjualan'));
     }
 }
